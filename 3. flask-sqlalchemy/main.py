64c79afb89d829afad05902da872f5f85245f66e
PinguinCake <cherry-crown@yandex.ru>
2022-03-10 16:29:04 +0300

    /

--- 3. flask-sqlalchemy/main.py
+++ 3. flask-sqlalchemy/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
-from data.forms import RegisterForm, LoginForm
+from data.forms import RegisterForm, LoginForm, WorksForm
 from flask_login import LoginManager, login_user, login_required, logout_user
 
 app = Flask(__name__)
@@ -70,6 +70,23 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/add_work', methods=['GET', 'POST'])
+def add_work():
+    form = WorksForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        job = Jobs(
+            team_leader=form.team_leader.data,
+            job=form.job.data,
+            work_size=form.work_size.data,
+            collaborators=form.collaborators.data
+        )
+        db_sess.add(job)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('add_work.html', title='Добавление работ', form=form)
 
 
 def main():
